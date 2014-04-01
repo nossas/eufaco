f0ed51225bbd13f4c4e2542aa2beda01e2acd351
Josias Schneider <josias.ds@gmail.com>
2014-04-01 14:23:10 -0300

    [#68570426] add form to request membership; add create user action

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,10 @@
 class Request < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true, uniqueness: true
+
+  def self.create_from_user params
+    user = User.find_by_email(params[:email])
+    user = User.create(params) if user.nil?
+    Request.where("user_id = ?", user.id).first_or_create(user_id: user.id)
+  end
 end
