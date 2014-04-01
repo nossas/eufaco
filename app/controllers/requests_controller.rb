c5a0d9c5640b52fd5f76bc2a71aead4b50d60bea
Josias Schneider <josias.ds@gmail.com>
2014-04-01 15:40:51 -0300

    [#68570426] send user ip in Meu Rio Accounts call

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,7 @@
 class RequestsController < InheritedResources::Base
 
   def create
+    params[:ip] = request.remote_ip
     request = Request.create_from_user(request_params)
     redirect_to root_path
   end
